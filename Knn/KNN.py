bcd59a7c04c0719b555797e2114a66c8c9146922
Sateesh Sivakoti <35126240+sat14Siv@users.noreply.github.com>
2018-06-14 17:28:58 +0530

    Added Comments

--- Knn/KNN.py
+++ Knn/KNN.py
@@ -15,60 +15,55 @@ def get_data():
     data = pd.read_csv('iris.csv', header = None)
     return data 
 
-def get_splits(data):
+def get_splits(data):     # Dividing the data into training and validation splits
     train_data, test_data = train_test_split(data, random_state = 42)
     validation_data = test_data.copy()
     validation_class = validation_data.loc[:,4]
     validation_data.drop(4, axis = 1, inplace = True)
     return train_data, validation_data, validation_class
 
-def eucledian_distance(instance, training_point):
+def euclidian_distance(instance, training_point):  # Using Euclidean to measure distance between points 
     squaredDistance = np.square(instance - training_point).sum()
     return np.sqrt(squaredDistance)
 
-def get_nearest_neighbors(instance, train_data, k):
+def get_nearest_neighbors(instance, train_data, k): # Returns the nearest neighbors of an instance
     distance, referenceIndex = [], []
     for i in range(len(train_data)):
-        distance.append(eucledian_distance(instance, train_data.iloc[i][train_data.columns[:-1]]))
+        distance.append(euclidian_distance(instance, train_data.iloc[i][train_data.columns[:-1]]))
         referenceIndex.append(train_data.index[i])
     distanceFrame = pd.DataFrame({'ReferenceIndex': referenceIndex, 'Distance':distance})       
     distanceFrame.sort_values(by = 'Distance', axis = 0, kind = 'quicksort', ascending = True, inplace = True)
     nearestNeighborsIndex = distanceFrame['ReferenceIndex'][:k]
     nearestNeighbors = train_data.loc[nearestNeighborsIndex]
-    #print(distanceFrame)
-    #print(nearestNeighbors)
     return nearestNeighbors
     
-def get_predictions(sample, train_data, k):
+def get_predictions(sample, train_data, k):   # Returns the predicted target/ class of an instance
     nearestNeighbors = get_nearest_neighbors(sample, train_data, k)
-    prediction = nearestNeighbors[4].value_counts().index[0]
+    prediction = nearestNeighbors[4].value_counts().index[0] # Most ocurring class in the neighbors is returned
     return prediction
 
 def get_accuracy(predictions, validation_class):
     validation_class = validation_class.reset_index()
     correct = 0
-    #print(predictions)
     for i in range(len(predictions)):
         if predictions[i] == validation_class.iloc[i][4]:
             correct+=1
     return 100*correct/len(predictions)
     
-def optimal_k(train_data, validation_data, validation_class):
+def optimal_k(train_data, validation_data, validation_class, k_min, k_max): 
+# Function to find out the optimal k to be used. Evaluates the validation accuracy for each k and returns the k with maximum accuracy.
     scores = []
-    for k in range(2,5):
+    for k in range(k_min, k_max+1):
         predictions = []
         for sample_index in range(len(validation_data)):
             sample = validation_data.iloc[sample_index]
-            #print(sample)
             predictions.append(get_predictions(sample, train_data, k))
-        #print(predictions)
         scores.append(get_accuracy(predictions, validation_class))
-        print(k,':',scores[-1])
-    k_req = scores.index(max(scores)) + 7
-    print(scores)
+        #print(k,':',scores[-1])
+    k_req = scores.index(max(scores)) + k_min
     return k_req
 
-def knn_prediction(train_data, test_data, k):
+def knn_prediction(train_data, test_data, k): # After finding out the optimal k for the data, this is use to classify the test data.
     predictions = []
     for sample_index in range(len(validation_data)):
         sample = validation_data.iloc[sample_index]
@@ -79,6 +74,8 @@ def knn_prediction(train_data, test_data, k):
 if __name__ == '__main__':
     data = get_data()
     train_data, validation_data, validation_class = get_splits(data)
-    k_req = optimal_k(train_data, validation_data, validation_class)
+    k_req = optimal_k(train_data, validation_data, validation_class, 2, 5)
+    # Now, use the test data. In mmy case, there is no test data
     predictions = knn_prediction(train_data, validation_data, k_req)
-    accur = get_accuracy(predictions, validation_class)    
+    #accur = get_accuracy(predictions, validation_class)  
+    #print(accur)
